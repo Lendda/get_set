2223b370ec144b90d524b7316ad03e98d1990044
BaronKlauss <toninho.juninho16@gmail.com>
2016-09-15 14:43:26 -0300

    sdfghjkldfghjk
    sdfghjk

--- src/get_set/Get_set.java
+++ src/get_set/Get_set.java
@@ -15,7 +15,7 @@ public class Get_set {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //hdjbcjsdbicjbiasdjnavbijkbjikjhbiujojnojnijnbiljnijbijbijbijhijhijhijjjjjjjjjjjjjjjjhkjkkoikkjji
+        //e ai jdfjdhfohjaon
     }
     
 }
